f49b27140e0d74d36d78537d5aa0d9c1779ea99b
difx99 <88432799+difx99@users.noreply.github.com>
2021-12-21 19:44:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/difx99/Player /Player
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/difx99/Player -b $BRANCH /Player
 fi
-cd /VCPlayerBot
+cd /Player
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
